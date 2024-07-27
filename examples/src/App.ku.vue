e2101c166849821b027d47dfdd747443c7f79eb1
Skiyee <319619193@qq.com>
2024-07-27 16:28:27 +0800

    examples: 🏀 采用最新使用方式的例子

---
+++ examples/src/App.ku.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const HelloVueRef = ref('test')
+</script>
+
+<template>
+  <div>Hello AppKuVue</div>
+  <KuRootView />
+  <div>{{ HelloVueRef }}</div>
+  <GlobalToast />
+</template>
--- examples/src/pages/index.vue
+++ examples/src/pages/index.vue
@@ -9,6 +9,10 @@ function handleClick() {
 </script>
 
 <template>
+  <!-- 这行代码仅小程序可见，完全支持 PageMeta -->
+  <page-meta>
+    <navigation-bar>Hi, Page Index</navigation-bar>
+  </page-meta>
   <view>
     Hello KuRoot
   </view>
