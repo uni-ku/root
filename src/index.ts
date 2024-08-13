e8c1517b88791f685274e52d7f0006d735b185bf
Skiyee <319619193@qq.com>
2024-08-13 11:32:37 +0800

    fix: 🩹 修复新增页面KuRoot无效问题

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { resolve } from 'node:path'
 import type { Plugin } from 'vite'
 import { createFilter } from 'vite'
 
+import chokidar from 'chokidar'
+
 import { loadPagesJson } from './utils'
 import { rebuildKuApp, registerKuApp } from './root'
 import { transformPage } from './page'
@@ -12,11 +14,19 @@ export default function UniKuRoot(): Plugin {
   const rootPath = process.env.UNI_INPUT_DIR || (`${process.env.INIT_CWD}\\src`)
   const appKuPath = resolve(rootPath, 'App.ku.vue')
   const pagesPath = resolve(rootPath, 'pages.json')
-  const pagesJson = loadPagesJson(pagesPath, rootPath)
+
+  let pagesJson = loadPagesJson(pagesPath, rootPath)
 
   return {
     name: 'vite-plugin-uni-root',
     enforce: 'pre',
+    buildStart() {
+      chokidar.watch(pagesPath).on('all', (event) => {
+        if (['add', 'change'].includes(event)) {
+          pagesJson = loadPagesJson(pagesPath, rootPath)
+        }
+      })
+    },
     async transform(code, id) {
       const filterMain = createFilter(`${rootPath}/main.(ts|js)`)
       if (filterMain(id))
